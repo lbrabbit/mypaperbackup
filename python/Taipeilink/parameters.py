239259b609af3b65c29f8507bcfbd95d3e2190ec
Haggen <haggenso@gmail.com>
2015-03-10 16:39:18 +0800

    Taipeilink v0.01

---
+++ python/Taipeilink/parameters.py
@@ -0,0 +1,8 @@
+url_base       ='http://guest.taipeilink.net/'
+url_css        =''
+url_css_list   =[]
+url_board      =''
+url_var        ='?pg='
+url_differ     =1
+read_size      =256
+socket_timeout =120
---
+++ python/Taipeilink/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+import py2exe
+
+setup(name="main",
+      version="0.02",
+      windows=["main.py"])
+
